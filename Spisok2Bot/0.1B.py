5fbafa2503489e8382ebfb6fbf920a6cd5d63e1e
s3tim <xtimofey11@mail.ru>
2025-08-07 19:32:29 +0300

    01.1

--- Spisok2Bot/0.1B.py
+++ Spisok2Bot/0.1B.py
@@ -1,15 +1,13 @@
 import telebot
 from telebot import TeleBot, types
 import time
+from openpyxl import load_workbook
 
 bot = telebot.TeleBot("7970047574:AAEL7j4lsTYbRzE4dYa7YEm2LYSzZh2o-Pg")
+wb = load_workbook('BackUp.xlsx')
 
-#Для состояния
-SOS = ["SOS"]  # Изначально в главном меню
-
-#Для нуммерации задач
-TaskState = ["TaskState"]
-user_data = {} 
+#Для состояния и нуммерации задач
+SOS = ["SOS", "TaskState"]  # Изначально в главном меню
 
 #Для всех слов бота
 Hi = [
@@ -20,7 +18,7 @@ Hi = [
     "Хорошо, теперь выбери количество\nЕсли твоих задачь больше, просто напиши их количество в чат",
     "Теперь отправь свои задачи",
     "верно?",
-    "Напишите новое число"
+    "Напишите новое число",
 ]
 
 @bot.callback_query_handler(func=lambda call: call.data == "edit_text1")
@@ -48,50 +46,63 @@ def start(message):
 #Здесь код для режима кнопок
 @bot.message_handler(func=lambda message: SOS[0] == "edit_text1")
 def button(message):
-    markup = types.InlineKeyboardMarkup()
-    markup.row(
+    markup1 = types.InlineKeyboardMarkup()
+    markup1.row(
         types.InlineKeyboardButton("1", callback_data="task1"),
         types.InlineKeyboardButton("2", callback_data="task2"),
         types.InlineKeyboardButton("3", callback_data="task3"),
         types.InlineKeyboardButton("4", callback_data="task4"),
         types.InlineKeyboardButton("5", callback_data="task5")
     )
+    if SOS[0] == "edit_text1":
+        SOS[0] = "SOS"
+        bot.send_message(message.chat.id, Hi[4], reply_markup=markup1)
+        return
 
-    if message.text.isdigit():  #если что-то из этого есть в сообщении оно проходит дальше
-        SOS[0] = f"task{message.text}"
-        TaskState[0] = f"{message.text}"
+@bot.message_handler()
+def button2(message):
+    if SOS[0] == "SOS" and message.text.isdigit():
+        task_num = message.text
         markup2 = types.InlineKeyboardMarkup()
         markup2.row(
-            types.InlineKeyboardButton("Да", callback_data=f"task{message.text}"),
-            types.InlineKeyboardButton("Нет", callback_data="task_button")
+            types.InlineKeyboardButton("Да", callback_data=f"task{task_num}"),
+            types.InlineKeyboardButton("Нет", callback_data="t_button")
         )
+        SOS[0] = f"task{task_num}"
+        SOS[1] = f"{task_num}"
         bot.send_message(message.chat.id, text=f"{message.text} {Hi[6]}", reply_markup=markup2)
     
-    if SOS[0] == "edit_text1":
-        bot.send_message(message.chat.id, Hi[4], reply_markup=markup)
+    for i in range(1, 10000):
+        if SOS[1] == f"{i}" and SOS[0] == "t_but":
+            SOS[0] = "call"
+            SOS[1] = f"{i}"
+            break
 
+    for i in range(1, 10000):
+        if SOS[1] in [f"{i}"] and SOS[0] == "call":
+            SOS[0] = "SOS"
+            SOS[1] = f"{i}"
+            bot.send_message(message.chat.id, SOS[1])
+            break
+            
 @bot.callback_query_handler(func=lambda call: call.data.startswith("task"))
 def callback_task(call):
-    #bot.send_message(call.message.chat.id, "WW")
-    #if call.data == "task_button":
-    #    bot.send_message(call.message.chat.id, Hi[7])
-    #    #TaskState[0] == f"{call.message.text}"
-    #    SOS[0] = f"task{call.message.text}"
+    SOS[0] = "task"
+    SOS[1] = f"{call.data}"
+    bot.edit_message_text(Hi[3], chat_id=call.message.chat.id, message_id=call.message.message_id)
+    for i in range(1, 10000):
+        if call.data == f"task{i}": #call.data это как message.text
+            SOS[0] = "call"
+            SOS[1] = f"{i}"
+            break
 
-    #if call.data == "task1":
-    #    SOS[0] = f"task1"
-    #    markup3 = types.InlineKeyboardMarkup()
-    #    markup3.row(
-    #        types.InlineKeyboardButton("Да", callback_data=f"task1"),
-    #        types.InlineKeyboardButton("Нет", callback_data="task_button")
-    #    )
-    #    bot.send_message(message.chat.id, Hi[5], reply_markup=markup3)
-    user_data[message.chat.id] = message.text
-    
+    button2(call.message)      #СУПЕР НАДО ставь эту тему вроде для перехода
 
-def test(message):
-    saved_text = user_data.get(call.message.chat.id, "❌ Данные не найдены")
-    bot.send_message(call.message.chat.id, f"Ваш текст: {saved_text}")
+@bot.callback_query_handler(func=lambda call: call.data.startswith("t_button")) #ЭТО ПИШИ НА НЕТ
+def callback_task(call):
+    SOS[0] = "t_but"
+    bot.edit_message_text(Hi[7], chat_id=call.message.chat.id, message_id=call.message.message_id)
+    button2(call.message)
 
 #Здесь код для текстового режима
 @bot.message_handler(func=lambda message: SOS[0] == "edit_text2")
@@ -99,3 +110,6 @@ def text(message):
     bot.send_message(message.chat.id, "text2")
 
 bot.infinity_polling()
+
+#ДОБАВЬ УДАЛЕНИЕ 
+#И ТАБЛИЦЫ
